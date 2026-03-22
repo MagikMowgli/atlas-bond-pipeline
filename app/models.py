5e657931b1c8013237d2e033ff5065f195dae22b
Mahdi <86474206+MagikMowgli@users.noreply.github.com>
2026-03-22 22:56:43 +0000

    completed models.py

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field, validator
+from datatime import datetime
+from typing import Optional
+
+class Bondposition(BaseModel):
+    isin: str = Field(..., min_length=12, max_length=12)
+    ticker: str
+    currency: str 
+
+    face_value: float = Field(..., gt=0)
+    clean_price: float = Field(..., gt=0)
+
+    timestamp: datetime = Field(default_factory=datetime.now)
+
+    @validator('currency')
+    def currency_must_be_uppercase(cls,v):
+        return v.upper()
+
+class MarketState(BaseModel):
+    currency: str
+    fx_rate_to_usd: float
