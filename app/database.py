973a6abfe8c4c6272b9334dcdd599af52779a12c
Mahdi <86474206+MagikMowgli@users.noreply.github.com>
2026-03-23 16:15:33 +0000

    added database.py

---
+++ app/database.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import datetime
+import os
+import dotenv import load_dotenv
+
+load_dotenv()
+# creates the physical connection to the database but not yet calling the db
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+if not SQL_ALCHEMY_DATABASE_URL:
+    raise ValueError("CRITICAL ERROR: DATABASE_URL not found in environment!")
+
+# this connects our python code to the sql database aka postgres
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
+# import to create sessions before we add to our database to keep things neat and orderly
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# allows us to model against a modelled pre written table
+Base = declarative_base()
+
+# builds the blueprint to the table we want
+class BondTable(Base):
+    __tablename__ = "bonds"
+
+    id = Column(Integer, primary_key=True, index=True)
+    isin = Column(String, index=True)
+    ticker = Column(String)
+    currency = Column(String)
+    face_value = Column(Float)
+    clean_price = Column(Float)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+# 5. The "Builder" Function essnetially builds the db in psql
+def init_db():
+    Base.metadata.create_all(bind=engine)
