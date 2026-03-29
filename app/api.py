34653b6db555e09a9a12ce8e88592974d44b3e3f
Mahdi <86474206+MagikMowgli@users.noreply.github.com>
2026-03-29 18:54:44 +0100

    post logic added

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
-from app.schemas import BondResponse
+from datetime import datetime
+from app.schemas import BondResponse, PortfolioSummary, BondCreate
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from typing import List
 from app.database import SessionLocal, BondTable
 
 app = FastAPI(title="Atlas Trade API")
@@ -14,11 +14,17 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/portfolio", response_model=List[BondResponse])
-def read_all_bonds(db: Session = Depends(get_db)) -> List[BondResponse]:
-    """Endpoint to retrieve all bonds in the portfolio."""
-    bonds = db.query(BondTable).all()
-    return bonds
+@app.get("/portfolio", response_model=PortfolioSummary)
+def read_all_portfolio(db: Session = Depends(get_db)) -> PortfolioSummary:
+    all_bonds = db.query(BondTable).all()
+
+    total_val = sum(bond.market_value for bond in all_bonds)
+
+    return {
+        "total_market_value": total_val,
+        "bond_count": len(all_bonds),
+        "bonds": all_bonds
+    }
 
 @app.get("/portfolio/{ticker}", response_model=BondResponse)
 def read_bond_by_ticker(ticker: str, db: Session = Depends(get_db)) -> BondResponse:
@@ -28,3 +34,21 @@ def read_bond_by_ticker(ticker: str, db: Session = Depends(get_db)) -> BondRespo
     if not bond:
         raise HTTPException(status_code=404, detail=f"Bond with ticker {ticker} not found")
     return bond
+
+@app.post("/portfolio/trade", response_model=BondResponse)
+def create_trade(bond_data: BondCreate, db: Session = Depends(get_db)):
+    new_bond_row = BondTable(
+        isin=bond_data.isin,
+        ticker=bond_data.ticker,
+        face_value=bond_data.face_value,
+        clean_price=bond_data.clean_price,
+        market_value=bond_data.face_value * (bond_data.clean_price / 100.0),
+        currency="USD",
+        valuation_timestamp=datetime.now()
+    )
+
+    db.add(new_bond_row)
+    db.commit()
+    db.refresh(new_bond_row)
+    
+    return new_bond_row
--- app/schemas.py
+++ app/schemas.py
@@ -11,3 +11,15 @@ class BondResponse(BaseModel):
 
     class Config:
         from_attributes = True # This is the "bridge" between SQLAlchemy and Pydantic
+
+class PortfolioSummary(BaseModel):
+    total_market_value: float
+    bond_count: int
+    bonds: list[BondResponse]
+
+class BondCreate(BaseModel):
+    isin: str
+    ticker: str
+    face_value: float
+    clean_price: float
+    currency: str = "USD"
