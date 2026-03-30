4aa5dc66a67f275c1826e22ee7ed7a4776acf119
Mahdi <86474206+MagikMowgli@users.noreply.github.com>
2026-03-30 21:02:31 +0100

    creating excel interface

--- excel_interface.py
+++ excel_interface.py
@@ -1,1 +1,9 @@
 import xlwings as xw
+import requests
+
+def book_trade_from_excel():
+    wb = xw.Book.caller()  # Connect to the Excel workbook that called this function
+    sheet = wb.sheets['Dashboard']  # the sheet which we're reading from
+
+    
+
